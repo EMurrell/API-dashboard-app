6397f08a028e00f238111e8d4d4af4d92218b051
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-02-09 10:51:52 -0500

    add custom max-width class

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="flex min-h-screen flex-col jsutify-center p-6 lg:p-24">
+     <div className="max-w-8xl"></div>
     </main>
   );
 }
